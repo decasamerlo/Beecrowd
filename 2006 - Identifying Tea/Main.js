79bf3f9de2dbf98b0399a3cdb6f03c7a85f8b59f
decasamerlo <casagrandamerlo@gmail.com>
2022-12-13 15:18:46 -0200

    2006 Added Javascript solution

---
+++ 2006 - Identifying Tea/Main.js
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('./dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+var T = lines.shift().trim();
+var answers = lines.shift();
+console.log(answers.split(T).length - 1);
