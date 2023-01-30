42975965323622f20bf0d9baa2ff427278ca2bb5
Andre <casagrandamerlo@gmail.com>
2023-01-30 12:59:40 -0300

    2434 Added Java solution

---
+++ 2434 - Saldo do Vovô/Main.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * IMPORTANT:
+ * O nome da classe deve ser "Main" para que a sua solução execute
+ * Class name must be "Main" for your solution to execute
+ * El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+
+        FastReader fr = new FastReader();
+        int n = fr.nextInt();
+        int s = fr.nextInt();
+        int min = s;
+
+        for (int i = 0; i < n; i++) {
+            s += fr.nextInt();
+            if (min > s) {
+                min = s;
+            }
+        }
+
+        System.out.println(min);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+
+}
